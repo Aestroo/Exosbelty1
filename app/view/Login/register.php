95a809fc384934a91a27d16052b0a9fa42c635a4
unknown <dhimasputrawan@gmail.com>
2024-01-23 14:09:39 +0700

    commit pertama

---
+++ app/view/Login/register.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="style.css">
+    <title>Sign Up</title>
+</head>
+
+<body>
+    <div class="header">
+        <div class="title">OUR TASK</div>
+        <div class="sub-title general-text-font">Task Management</div>
+    </div>
+    <div class="container">
+        <div class="login general-text-font">Create your account</div>
+        <div class="input-box">
+            <div class="logo">
+                <svg class="profile" width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6.04998 8.56306C8.87008 8.56306 11.25 9.02131 11.25 10.7893C11.25 12.558 8.85448 13 6.04998 13C3.23052 13 0.849976 12.5417 0.849976 10.7737C0.849976 9.00506 3.24547 8.56306 6.04998 8.56306ZM6.04998 0C7.96039 0 9.49107 1.53012 9.49107 3.43918C9.49107 5.34825 7.96039 6.87902 6.04998 6.87902C4.14021 6.87902 2.60889 5.34825 2.60889 3.43918C2.60889 1.53012 4.14021 0 6.04998 0Z" fill="white" />
+                </svg>
+            </div>
+            <input type="number" placeholder="NIS" class="input">
+        </div>
+        <div class="input-box">
+            <div class="logo">
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" viewBox="0 0 18 16" fill="none">
+                    <path d="M13.1448 0.5C14.2623 0.5 15.3373 0.941667 16.1282 1.73417C16.9198 2.525 17.3623 3.59167 17.3623 4.70833V11.2917C17.3623 13.6167 15.4707 15.5 13.1448 15.5H4.91235C2.58651 15.5 0.695679 13.6167 0.695679 11.2917V4.70833C0.695679 2.38333 2.57818 0.5 4.91235 0.5H13.1448ZM14.0873 4.83333C13.9123 4.82417 13.7457 4.88333 13.6198 5L9.86234 8C9.37901 8.40083 8.68651 8.40083 8.19568 8L4.44568 5C4.18651 4.80833 3.82818 4.83333 3.61235 5.05833C3.38735 5.28333 3.36235 5.64167 3.55318 5.89167L3.66235 6L7.45401 8.95833C7.92068 9.325 8.48651 9.525 9.07901 9.525C9.66985 9.525 10.2457 9.325 10.7115 8.95833L14.4707 5.95L14.5373 5.88333C14.7365 5.64167 14.7365 5.29167 14.5282 5.05C14.4123 4.92583 14.2532 4.85 14.0873 4.83333Z" fill="white" />
+                </svg>
+            </div>
+            <input type="email" id="" placeholder="Email" class="input">
+        </div>
+        <div class="input-box">
+            <div class="logo">
+                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="18" viewBox="0 0 15 18" fill="none">
+                    <path d="M7.07588 0.731476C9.63316 0.731476 11.6908 2.7438 11.6908 5.22815V6.50593C13.1261 6.95394 14.1719 8.25326 14.1719 9.80515V13.9192C14.1719 15.8405 12.5791 17.3981 10.6154 17.3981H3.56258C1.59805 17.3981 0.00524902 15.8405 0.00524902 13.9192V9.80515C0.00524902 8.25326 1.05187 6.95394 2.48632 6.50593V5.22815C2.49479 2.7438 4.55247 0.731476 7.07588 0.731476ZM7.08435 10.2184C6.67789 10.2184 6.34765 10.5413 6.34765 10.9388V12.7773C6.34765 13.183 6.67789 13.506 7.08435 13.506C7.49927 13.506 7.82952 13.183 7.82952 12.7773V10.9388C7.82952 10.5413 7.49927 10.2184 7.08435 10.2184ZM7.09282 2.18068C5.37385 2.18068 3.97666 3.53879 3.96819 5.21159V6.32623H10.209V5.22815C10.209 3.54707 8.81178 2.18068 7.09282 2.18068Z" fill="white" />
+                </svg>
+            </div>
+            <div class="input password">
+                <input type="password" id="password" placeholder="Password">
+                <span class="material-symbols-outlined" id="toggleIcon">&#xe8f5;</span>
+            </div>
+        </div>
+        <div class="input-box">
+            <div class="logo">
+                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="18" viewBox="0 0 15 18" fill="none">
+                    <path d="M7.07588 0.731476C9.63316 0.731476 11.6908 2.7438 11.6908 5.22815V6.50593C13.1261 6.95394 14.1719 8.25326 14.1719 9.80515V13.9192C14.1719 15.8405 12.5791 17.3981 10.6154 17.3981H3.56258C1.59805 17.3981 0.00524902 15.8405 0.00524902 13.9192V9.80515C0.00524902 8.25326 1.05187 6.95394 2.48632 6.50593V5.22815C2.49479 2.7438 4.55247 0.731476 7.07588 0.731476ZM7.08435 10.2184C6.67789 10.2184 6.34765 10.5413 6.34765 10.9388V12.7773C6.34765 13.183 6.67789 13.506 7.08435 13.506C7.49927 13.506 7.82952 13.183 7.82952 12.7773V10.9388C7.82952 10.5413 7.49927 10.2184 7.08435 10.2184ZM7.09282 2.18068C5.37385 2.18068 3.97666 3.53879 3.96819 5.21159V6.32623H10.209V5.22815C10.209 3.54707 8.81178 2.18068 7.09282 2.18068Z" fill="white" />
+                </svg>
+            </div>
+            <div class="input password">
+                <input type="password" id="confirm-password" placeholder="Confirm Password" style="flex-grow: 1;">
+                <span class="material-symbols-outlined" id="toggleIcon-2">&#xe8f5;</span>
+            </div>
+        </div>
+    </div>
+    <a href="asdasad.php" class="general-text-font forgot-password">Forgot password?</a>
+    <button class="general-text-font login-btn" onclick="window.location.href='mainmenu.html'">Login</button>
+    <div class="sign-up">
+        <label for="" class="general-text-font sign-up-text">Don't have an account?</label>
+        <a href="index.html" class="general-text-font sign-up-text">Sign In</a>
+    </div>
+    <script src="script.js"></script>
+</body>
+
+</html>
